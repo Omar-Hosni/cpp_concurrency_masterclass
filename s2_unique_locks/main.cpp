6f93df6a02d5ed32343218f1520091f99fc33f7f
Omar Hosny <68732608+Omar-Hosni@users.noreply.github.com>
2021-09-15 17:19:30 +0300

    Update main.cpp

--- s2_unique_locks/main.cpp
+++ s2_unique_locks/main.cpp
@@ -1,97 +1,117 @@
 #include <iostream>
-#include <mutex>
 #include <thread>
+#include <mutex>
 #include <string>
+#include <chrono>
 
+using namespace std;
 
-/***************************************************Example 1 ******************************************/
 class bank_account {
 	double balance;
-	std::string name;
-	std::mutex m;
+	string name;
+	mutex m;
 
 public:
 	bank_account() {};
 
-	bank_account(double _balance, std::string _name) :balance(_balance), name(_name) {}
+	bank_account(double b, string n) :balance(b), name(n) {};
 
 	bank_account(bank_account& const) = delete;
+
 	bank_account& operator=(bank_account& const) = delete;
 
-	void withdraw(double amount)
-	{
-		std::lock_guard<std::mutex> lg(m);
+	void withdraw(double amount) {
+		lock_guard<mutex> lg(m);
 		balance += amount;
 	}
 
-	void deposite(double amount)
-	{
-		std::lock_guard<std::mutex> lg(m);
+	void deposit(double amount) {
+		lock_guard<mutex> lg(m);
 		balance += amount;
 	}
 
-	void transfer(bank_account& from, bank_account& to, double amount)
-	{
+	void transfer(bank_account& from, bank_account& to, double amount) {
+		
+		unique_lock<mutex> ul_1(from.m, defer_lock);
+		unique_lock<mutex> ul_2(to.m, defer_lock);
+		lock(ul_1, ul_2);
 
-		std::cout << std::this_thread::get_id() << " hold the lock for both mutex \n";
-
-		std::unique_lock<std::mutex> ul_1(from.m, std::defer_lock);
-		std::unique_lock<std::mutex> ul_2(to.m, std::defer_lock);
-		std::lock(ul_1, ul_2);
-
+		
 		from.balance -= amount;
 		to.balance += amount;
-		std::cout << "transfer to - " << to.name << "   from - " << from.name << "  end \n";
+		cout << "transfer to - " << to.name << " from - " << from.name << " end\n";
 	}
+
 };
 
-void run_code1()
-{
+mutex m1;
+mutex m2;
 
+void m1_first_m2_second() {
+	lock_guard<mutex> lg1(m1);
+	this_thread::sleep_for(chrono::milliseconds(1000));
+	cout << "thread " << this_thread::get_id() << " has acquired lock for m1, its waiting for m2\n";
+
+	lock_guard<mutex> lg2(m2);
+	this_thread::sleep_for(chrono::milliseconds(1000));
+	cout << "thread " << this_thread::get_id() << " has acquired lock for m2 mutex \n";
+
+}
+
+void m2_first_m1_second() {
+	lock_guard<mutex> lg1(m2);	
+	this_thread::sleep_for(chrono::milliseconds(1500));
+	cout << "thread " << this_thread::get_id() << " has acquired lock for m2 mutex, its waiting for m1" << endl;
+
+	lock_guard<mutex>lg2(m1);
+	cout << "thread " << this_thread::get_id() << " has acquired lock for m1 mutex" << endl;
+}
+
+void x_operation() {
+	cout << "this is some operation\n";
+}
+
+void y_operation() {
+	cout << "this is some other operations \n";
+}
+
+unique_lock<mutex>get_lock() {
+	mutex m;
+	unique_lock<mutex> ul(m);
+	x_operation();
+	return ul;
+}
+
+int main() {
+
+	//uncomment whatever you want to execute
+	// 
+	//run_code_1
 	bank_account account;
 
-	bank_account account_1(1000, "james");
+	bank_account account_1(1000, "James");
 	bank_account account_2(2000, "Mathew");
 
-	std::thread thread_1(&bank_account::transfer, &account, std::ref(account_1), std::ref(account_2), 500);
-	std::thread thread_2(&bank_account::transfer, &account, std::ref(account_2), std::ref(account_1), 200);
+	//thread thread1(&bank_account::transfer, &account, ref(account_1), ref(account_2), 500);
+	//this_thread::sleep_for(chrono::milliseconds(100));
 
-	thread_1.join();
-	thread_2.join();
-}
+	//thread thread2(&bank_account::transfer, &account, ref(account_2), ref(account_1), 200);
 
+	//thread1.join();
+	//thread2.join();
 
+	//run_code_2
+	//thread thread3(m1_first_m2_second);
+	thread thread4(m2_first_m1_second);
 
-/***************************************************Example 2 ******************************************/
+	//thread3.join();
+	//thread4.join();
 
-void x_operations()
-{
-	std::cout << "this is some operations \n";
-}
-
-void y_operations()
-{
-	std::cout << "this is some other operations \n";
-}
-
-std::unique_lock<std::mutex> get_lock()
-{
-	std::mutex m;
-	std::unique_lock<std::mutex> lk(m);
-	x_operations();
-	return lk;
-}
-
-void run_code2()
-{
-	std::unique_lock<std::mutex> lk(get_lock());
-	y_operations();
-}
-
-int main()
-{
-	run_code1();
-	run_code2();
+	//run_code_3
+	unique_lock<mutex> ul(get_lock());
+	y_operation();
 
 	return 0;
 }
+
+
